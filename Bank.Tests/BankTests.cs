58250886e199a92fbe1348f76ad922bf8fdc3068
Samuel Bering <samuelbering@hotmail.com>
2018-11-07 12:09:50 +0100

    Setup test project

---
+++ Bank.Tests/BankTests.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+using SamsWebBank.Models;
+
+namespace Bank.Tests
+{
+    public class BankTests
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            ErrorViewModel ev = new ErrorViewModel
+            {
+                RequestId = "100"
+            };
+
+            Assert.Equal("100", ev.RequestId);
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 3));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
